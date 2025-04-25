9a374869e2a261e6a5e9bdb6cc77568c53c231a4
Created by Nwadili <diliora.azukaonwuka@gmail.com>
2025-04-24 23:39:47 -0300

    Harmonised colour palette Added icons and new style elements Added project JSON file for easy updating Refined margins and spacing

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 document.getElementById("menu-toggle").addEventListener("click", function () {
     document.getElementById("nav-links").classList.toggle("show");
 });
@@ -17,3 +19,68 @@ function filterProjects() {
         }
     });
 }
+
+fetch('projects.json')
+  .then(response => {
+    if (!response.ok) throw new Error("Failed to load JSON");
+    return response.json();
+  })
+  .then(data => {
+    const container = document.getElementById('project-container');
+    data.forEach(project => {
+      const tags = project.tags.map(tag => `<span>${tag}</span>`).join(" ");
+      const projectHTML = `
+        <div class="project">
+
+          <div class="project-image">
+            <img src="${project.image}" alt="${project.alt || project.title}">
+          </div>
+
+          <div class="project-details">
+            <h3>${project.title}</h3>
+            <p>${project.description}</p>
+            <div class="tags">
+              ${tags}
+            </div>
+            <a href="${project.link}" class="view-code">View Code</a>
+          </div>
+          
+        </div>
+      `;
+      container.innerHTML += projectHTML;
+    });
+  })
+  .catch(error => {
+    console.error("Error loading project data:", error);
+  });
+
+
+  // on home page
+  fetch('projects.json')
+  .then(response => {
+    if (!response.ok) throw new Error("Failed to load JSON");
+    return response.json();
+  })
+  .then(data => {
+    const homeContainer = document.getElementById('project-grid');
+    data.forEach(project => {
+      const tags = project.tags.map(tag => `<span>${tag}</span>`).join(" ");
+      const homeProjectHTML = `
+        <div class="project-item">
+          <div>
+            <a href="${project.link}" target="blank">
+              <h3>${project.title}</h3>
+              <p>${project.shortDescription || project.description}</p>
+            </a>
+          </div>
+          <div class="tags">
+              ${tags}
+          </div>          
+        </div>
+      `;
+      homeContainer.innerHTML += homeProjectHTML;
+    });
+  })
+  .catch(error => {
+    console.error("Error loading project data:", error);
+  });
